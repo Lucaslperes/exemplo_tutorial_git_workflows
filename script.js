99336b2d944afd1a47bb94674b1fd8c32bed658b
Lucas L. Peres <llperes98@gmail.com>
2026-06-15 17:56:11 -0300

    Adiciona contador de livros

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+let totalLivros = 0;
+
+function cadastrarLivro() {
+    const titulo = document.getElementById("titulo").value;
+    const item = document.createElement("li");
+    item.textContent = titulo;
+    document.getElementById("listaLivros")
+        .appendChild(item);
+    totalLivros++;
+    document.getElementById("contador")
+        .textContent = totalLivros;
+}
+
 function mensagemBoasVindas() {
     document.getElementById("mensagem").textContent = "Bem-vindo ao Sistema de Biblioteca!";
 }
